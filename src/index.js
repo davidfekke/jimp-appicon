5c521dc5dfc80dec223290b38eebb3847e05782a
David Fekke <david@fekke.com>
2016-09-13 19:26:57 -0400

    Adding an image for tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
+var Jimp = require("jimp");
+const contents = require('../Contents.json');
+const filename = '../SF1Good_Marketing.png';
+
 /**
  * @param {Type}
  * @return {Type}
  */
 export default function () {
+  contents.images.forEach(i => {
+    console.log('Processing: ' + i.filename);
+    const width = i.size.split('x')[0];
+    const scale = i.scale.charAt(0);
+    const scaledSize = width * scale;
+    resizeAndSaveIcon(filename, i.filename, scaledSize);
+  });
   return true
+}
+
+
+
+function resizeAndSaveIcon(filename, name, size) {
+    Jimp.read(filename).then(function (icon) {
+    icon.resize(size, size)             // resize  
+        .write(name);                   // save 
+    }).catch(function (err) {
+        console.error(err);
+    });
 }
