f0335b1126aa882f361f4fbdf12a66214eedd00f
David Fekke <david@fekke.com>
2016-09-19 13:32:44 -0400

    Added artwork for marketing

--- src/appicon.js
+++ src/appicon.js
@@ -11,18 +11,21 @@ const contents = require(path.join(__dirname, '../Contents.json'));
 export default function (source) {
   const filepath = path.join(__dirname, source);
   const contentFile = path.join(__dirname, '../Contents.json');
-
-  fs.mkdir('Appicon.iconset', () => {
-    
-    fs.createReadStream(contentFile).pipe(fs.createWriteStream('Appicon.iconset/Contents.json'));
+  
+  fs.mkdir('Icons', () => {
+    resizeAndSaveIcon(source, 'Icons/iTunesArtwork@1x.png', 512);
+    resizeAndSaveIcon(source, 'Icons/iTunesArtwork@2x.png', 1024);
+    resizeAndSaveIcon(source, 'Icons/iTunesArtwork@3x.png', 1532);
+    fs.mkdir('Icons/Appicon.appiconset', () => {
+      fs.createReadStream(contentFile).pipe(fs.createWriteStream('Icons/Appicon.appiconset/Contents.json'));
       contents.images.forEach(i => {
-      console.log('Processing: ' + i.filename);
-      const width = i.size.split('x')[0];
-      const scale = i.scale.charAt(0);
-      const scaledSize = width * scale;
-      resizeAndSaveIcon(source, 'Appicon.iconset/' + i.filename, scaledSize);
+        console.log('Processing: ' + i.filename);
+        const width = i.size.split('x')[0];
+        const scale = i.scale.charAt(0);
+        const scaledSize = width * scale;
+        resizeAndSaveIcon(source, 'Icons/Appicon.appiconset/' + i.filename, scaledSize);
+      });
     });
-    
   });
   return true;
 }
